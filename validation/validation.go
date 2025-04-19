9e6982f1f11882bedd8bdbc11bccf9d27ddb081b
RusimbiPatrick <rusimbipatrick@outlook.com>
2025-04-19 15:05:43 +0200

    add validation

--- validation/validation.go
+++ validation/validation.go
@@ -1,1 +1,29 @@
 package validation
+
+import (
+	"regexp"
+)
+
+
+func IsValidPhoneNumber(phoneNumber string) bool{
+	//allow only  10 digits, spaces, and dashes.
+	phoneNumberPattern := `^\d{3}[-\s]?\d{3}[-\s]?\d{4}$`
+
+	matched, err := regexp.MatchString(phoneNumberPattern, phoneNumber)
+
+	if err != nil {
+		return false
+	}
+
+	return matched;
+}
+
+
+func IsValidEmail(email string) bool {
+	emailPattern := `[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`;
+	matched, err := regexp.MatchString(emailPattern, email)
+	if err != nil {
+		return false
+	}
+	return matched
+}
